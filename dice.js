6363416e554bdd962549223380fd37e6c4d1c800
Saad <badreddinesaad91@gmail.com>
2022-01-26 09:16:37 +0200

    fixing dice images

--- dice.js
+++ dice.js
@@ -3,31 +3,31 @@
 	var random2 = Math.floor(Math.random() * (7 - 1) + 1);
 
 	if (random1 == 1) {
-		document.getElementsByClassName('img1')[0].src = '/images/dice1.png';
+		document.getElementsByClassName('img1')[0].src = 'images/dice1.png';
 	} else if (random1 == 2) {
-		document.getElementsByClassName('img1')[0].src = '/images/dice2.png';
+		document.getElementsByClassName('img1')[0].src = 'images/dice2.png';
 	} else if (random1 == 3) {
-		document.getElementsByClassName('img1')[0].src = '/images/dice3.png';
+		document.getElementsByClassName('img1')[0].src = 'images/dice3.png';
 	} else if (random1 == 4) {
-		document.getElementsByClassName('img1')[0].src = '/images/dice4.png';
+		document.getElementsByClassName('img1')[0].src = 'images/dice4.png';
 	} else if (random1 == 5) {
-		document.getElementsByClassName('img1')[0].src = '/images/dice5.png';
+		document.getElementsByClassName('img1')[0].src = 'images/dice5.png';
 	} else {
-		document.getElementsByClassName('img1')[0].src = '/images/dice6.png';
+		document.getElementsByClassName('img1')[0].src = 'images/dice6.png';
 	}
 
 	if (random2 == 1) {
-		document.getElementsByClassName('img2')[0].src = '/images/dice1.png';
+		document.getElementsByClassName('img2')[0].src = 'images/dice1.png';
 	} else if (random2 == 2) {
-		document.getElementsByClassName('img2')[0].src = '/images/dice2.png';
+		document.getElementsByClassName('img2')[0].src = 'images/dice2.png';
 	} else if (random2 == 3) {
-		document.getElementsByClassName('img2')[0].src = '/images/dice3.png';
+		document.getElementsByClassName('img2')[0].src = 'images/dice3.png';
 	} else if (random2 == 4) {
-		document.getElementsByClassName('img2')[0].src = '/images/dice4.png';
+		document.getElementsByClassName('img2')[0].src = 'images/dice4.png';
 	} else if (random2 == 5) {
-		document.getElementsByClassName('img2')[0].src = '/images/dice5.png';
+		document.getElementsByClassName('img2')[0].src = 'images/dice5.png';
 	} else {
-		document.getElementsByClassName('img2')[0].src = '/images/dice6.png';
+		document.getElementsByClassName('img2')[0].src = 'images/dice6.png';
 	}
 }),
 	true;
